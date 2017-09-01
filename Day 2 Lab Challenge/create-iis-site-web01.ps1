a20d6a6c548da50badd4ccd3dbd0b3eb6b617490
gavinmcshera <gavin@mcshera.com>
2017-09-01 13:45:36 +0100

    Update create-iis-site-web01.ps1

--- Day 2 Lab Challenge/create-iis-site-web01.ps1
+++ Day 2 Lab Challenge/create-iis-site-web01.ps1
@@ -61,7 +61,7 @@
           <div style="border: 2px solid #8AC007; border-radius: 25px; padding: 20px; margin: 10px; width: 650px;">
             <b>Image File Linked from the Internet</b>:<br />
             <br />
-            <img src="http://mcshera.com/wp-content/uploads/2015/11/cropped-cropped-Gav-Logo-Final-512.jpg" alt="McShera Consulting"/>
+            <img src="http://mcshera.com/wp-content/uploads/2017/08/sublogo_gms.jpg" alt="McShera Consulting"/>
             </div>
         </div>
       </form>
