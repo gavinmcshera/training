aa21badc4b2599b307c0600ff63fd927e9fd0012
gavinmcshera <gavin@mcshera.com>
2017-09-15 10:49:58 +0100

    Update convertManagedDisksSingleVM.ps1

--- ManagedDisks/convertManagedDisksSingleVM.ps1
+++ ManagedDisks/convertManagedDisksSingleVM.ps1
@@ -15,6 +15,10 @@ $subscription = Read-Host -Prompt "Enter Subscription Name"
 $ResourceGroupName = Read-Host -Prompt "Enter target VM resource group"
 $vmName = Read-Host -Prompt "Enter target VM resource group"
 
+#Login to AzureRM and select subscription
+Login-AzureRmAccount
+Set-AzureRmContext -SubscriptionName $subscription
+
 #Stop the VM
 Stop-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $vmName -Force
 
