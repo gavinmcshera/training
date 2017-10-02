6c1a802452920525b0c2a518c6c4cca7668c2fe3
gavinmcshera <gavin@mcshera.com>
2017-10-02 17:53:21 +0100

    Folder Changes
    Folder changes

--- Day 2 Lab Challenge/enable-icmp.ps1
+++
@@ -1,4 +0,0 @@
-﻿# Turn On ICMPv4
-New-NetFirewallRule -Name Allow_ICMPv4 -DisplayName "Allow ICMPv4" -Protocol ICMPv4 -Enabled True -Profile Any -Action Allow
-# Allow tcp 8080 for the Default Web Site
-New-NetFirewallRule -Name "Default Website 8080" -Description "Binding to 8080" -DisplayName "Default Website 8080" -Enabled:True -Direction Inbound -Action Allow -Protocol TCP -LocalPort 8080
